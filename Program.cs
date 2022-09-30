8ad85a7d1563cbac00c262a8bb00693797d0b087
Foretack <observer.in.a.box@gmail.com>
2022-09-30 10:31:09 +0200

    Fix not logging verbose events

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ internal static class Program
     private static async Task Main()
     {
         Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Verbose()
             .WriteTo.File("verbose.txt", LogEventLevel.Verbose, "{Timestamp:HH:mm:ss zzz}-----[{Level}]➜ {Message:lj}{NewLine}", flushToDiskInterval: TimeSpan.FromMinutes(10), rollingInterval: RollingInterval.Day)
             .WriteTo.File("logs.txt", LogEventLevel.Debug, "{Timestamp:HH:mm:ss zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}", flushToDiskInterval: TimeSpan.FromMinutes(10), rollingInterval: RollingInterval.Day)
             .WriteTo.Console(LogEventLevel.Information)
